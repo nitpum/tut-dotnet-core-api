c67a3fdbe77e868a9bc5456c5541595ea8d8e3d3
nitpum <nitpum@outlook.com>
2020-10-08 01:07:23 +0700

    chore: Initialized project

---
+++ TuTNetCoreAPI/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TuTNetCoreAPI.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
